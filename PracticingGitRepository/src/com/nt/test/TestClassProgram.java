0db7189c34ec94b93b29d0bb3393820ca98a71c5
Lalit Kumar Rajak <user83@DESKTOP-3GR107M>
2022-12-05 11:56:05 +0530

    going forward to first stage

--- PracticingGitRepository/src/com/nt/test/TestClassProgram.java
+++ PracticingGitRepository/src/com/nt/test/TestClassProgram.java
@@ -4,6 +4,7 @@ public class TestClassProgram {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcomt to the First Stage of the git");
+		System.out.println("Staging is now Started");
 	}
 
 }
