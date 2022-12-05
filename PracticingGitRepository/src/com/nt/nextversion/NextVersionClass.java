5a88ab116992bb70bd1f2eabad97b66fec4e5def
Lalit Kumar Rajak <user83@DESKTOP-3GR107M>
2022-12-05 12:38:23 +0530

    all the files are up to date still check once if any problem in the project then confirm the  problem here

---
+++ PracticingGitRepository/src/com/nt/nextversion/NextVersionClass.java
@@ -0,0 +1,11 @@
+package com.nt.nextversion;
+
+import java.time.LocalDateTime;
+
+public class NextVersionClass {
+	
+	public void getNextVersion() {
+		System.out.println("Next Version Has Been Launched on :: "+LocalDateTime.now());
+	}
+
+}
---
+++ PracticingGitRepository/src/com/nt/second/SecondVersionOftTheProject.java
@@ -0,0 +1,9 @@
+package com.nt.second;
+
+public class SecondVersionOftTheProject {
+
+	
+	public void secondVersion(String user) {
+		System.out.println("Welcome to the Second Version of the project dear "+user);
+	}
+}
